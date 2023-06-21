fd9f9887e0ec08c16fae2fa2fb5a641d49d7c03b
Serenafeldberg <sfeldberg@udesa.edu.ar>
2023-06-21 18:33:37 -0300

    veer

--- cuadrados_minimos.py
+++ cuadrados_minimos.py
@@ -10,14 +10,28 @@ def cost_function_gradient(A, b, x):
     gradient = 2 * np.dot(A.T, error)
     return gradient
 
+def regularized_cost_gradient (A, b, x, delta):
+    gradient = cost_function_gradient(A, b, x) + 2 * delta * x
+    return gradient
 
-def gradient_descent (A, b, s, iterations, x0):
+def gradient_descent_F (A, b, s, iterations, x0):
     x = x0
-
     for _ in range (iterations):
         gradient = cost_function_gradient(A, b, x)
         x = x - s * gradient
+    return x
 
+def gradient_descent_F2 (A, b, s, delta, iterations, x0):
+    x = x0
+    for _ in range (iterations):
+        gradient = regularized_cost_gradient(A, b, x, delta)
+        x = x - s * gradient
+    return x
+
+def svd_least_squares(A, b):
+    U, S, Vt = np.linalg.svd(A, full_matrices=False)
+    S_pinv = np.diag(np.reciprocal(S))
+    x = Vt.T @ S_pinv @ U.T @ b
     return x
 
 def grad (A, x, b):
@@ -28,16 +42,16 @@ def learning_rate (A):
     return 1 / max_val
 
 if __name__ == "__main__":
-    A = np.random.rand(5, 100)
+    A = np.random.rand(5, 10)
     b = np.random.rand(5, 1)
 
-    print(A)
-    print(b)
-
     s = learning_rate(A)
-    x0 = np.random.rand(100, 1)
+    x0 = np.random.rand(10, 1)
 
-    x = gradient_descent(A, b, s, 1000, x0)
+    x = gradient_descent_F(A, b, s, 1000, x0)
+    print (x)
+
+    x = svd_least_squares(A, b)
     print (x)
 
 
